652d2d7397ac4c37a54bf4d2883d452f3eae4dad
Matthew Creek <matthew.creek27@gmail.com>
2023-01-12 23:08:15 -0600

    changed printJobs method to satisfy autograder

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -124,7 +124,7 @@ public class TechJobs {
             for (Map.Entry<String, String> listing : job.entrySet()){
                 System.out.println(listing.getKey() + ": " + listing.getValue());
             }
-            System.out.println("*****\n");
+            System.out.println("*****");
         }
     }
 }
